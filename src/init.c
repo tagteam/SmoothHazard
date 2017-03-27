c4aa631b2823417a2b2c10716fc332a6cbc0ff5c
Thomas Alexander Gerds <tag@biostat.ku.dk>
2017-03-27 13:17:45 +0200

    R_registerRoutines

---
+++ src/init.c
@@ -0,0 +1,27 @@
+#include <R_ext/RS.h>
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
+
+/* FIXME: 
+   Check these declarations against the C/Fortran source code.
+*/
+
+/* .Fortran calls */
+extern void F77_NAME(idmpl)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(idmweib)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(survpl)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+extern void F77_NAME(survweib)(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
+
+static const R_FortranMethodDef FortranEntries[] = {
+    {"idmpl",    (DL_FUNC) &F77_NAME(idmpl),    50},
+    {"idmweib",  (DL_FUNC) &F77_NAME(idmweib),  37},
+    {"survpl",   (DL_FUNC) &F77_NAME(survpl),   37},
+    {"survweib", (DL_FUNC) &F77_NAME(survweib), 29},
+    {NULL, NULL, 0}
+};
+
+void R_init_SmoothHazard(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, NULL, FortranEntries, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
